c4a2226dd5ca71949f1b8154d9418fcc2310ed58
Laurent85 <manceaulaurent@yahoo.fr>
2016-07-29 16:23:20 +0200

    bilan2

--- Notes collège/Bilan_Année.xaml.cs
+++ Notes collège/Bilan_Année.xaml.cs
@@ -19,6 +19,7 @@ namespace Notes_collège
     /// </summary>
     public partial class Bilan_Année : Window
         {
+        public string essai = "";
         public Bilan_Année()
             {
             InitializeComponent();
@@ -26,7 +27,59 @@ namespace Notes_collège
 
         private void Bilan_Loaded(object sender, RoutedEventArgs e)
             {
-            
+            Titre.Content = "Bilan - " + Principal.classe;
+            Lire_moyennes lire_moyennes = new Lire_moyennes();
+
+            lire_moyennes.Lecture_moyennes("Baptiste", Principal.classe);
+            moy1_Baptiste.Content = lire_moyennes.tri1.Content;
+            moy2_Baptiste.Content = lire_moyennes.tri2.Content;
+            moy3_Baptiste.Content = lire_moyennes.tri3.Content;
+            moy1_classe_Baptiste.Content = lire_moyennes.tri1_classe.Content;
+            moy2_classe_Baptiste.Content = lire_moyennes.tri2_classe.Content;
+            moy3_classe_Baptiste.Content = lire_moyennes.tri3_classe.Content;
+            lire_moyennes.Calcul_moyenne_générale("Baptiste", Principal.classe);
+            moy_annee_Baptiste.Content = lire_moyennes.annee.Content;
+            moy_annee_classe_Baptiste.Content = lire_moyennes.annee_classe.Content;
+            lire_moyennes.tri1.Content = "";
+            lire_moyennes.tri2.Content = "";
+            lire_moyennes.tri3.Content = "";
+            lire_moyennes.tri1_classe.Content = "";
+            lire_moyennes.tri2_classe.Content = "";
+            lire_moyennes.tri3_classe.Content = "";
+
+            lire_moyennes.Lecture_moyennes("Justine", Principal.classe);
+            moy1_Justine.Content = lire_moyennes.tri1.Content;
+            moy2_Justine.Content = lire_moyennes.tri2.Content;
+            moy3_Justine.Content = lire_moyennes.tri3.Content;
+            moy1_classe_Justine.Content = lire_moyennes.tri1_classe.Content;
+            moy2_classe_Justine.Content = lire_moyennes.tri2_classe.Content;
+            moy3_classe_Justine.Content = lire_moyennes.tri3_classe.Content;
+            lire_moyennes.Calcul_moyenne_générale("Justine", Principal.classe);
+            moy_annee_Justine.Content = lire_moyennes.annee.Content;
+            moy_annee_classe_Justine.Content = lire_moyennes.annee_classe.Content;
+            lire_moyennes.tri1.Content = "";
+            lire_moyennes.tri2.Content = "";
+            lire_moyennes.tri3.Content = "";
+            lire_moyennes.tri1_classe.Content = "";
+            lire_moyennes.tri2_classe.Content = "";
+            lire_moyennes.tri3_classe.Content = "";
+
+            lire_moyennes.Lecture_moyennes("Emilien", Principal.classe);
+            moy1_Emilien.Content = lire_moyennes.tri1.Content;
+            moy2_Emilien.Content = lire_moyennes.tri2.Content;
+            moy3_Emilien.Content = lire_moyennes.tri3.Content;
+            moy1_classe_Emilien.Content = lire_moyennes.tri1_classe.Content;
+            moy2_classe_Emilien.Content = lire_moyennes.tri2_classe.Content;
+            moy3_classe_Emilien.Content = lire_moyennes.tri3_classe.Content;
+            lire_moyennes.Calcul_moyenne_générale("Emilien", Principal.classe);
+            moy_annee_Emilien.Content = lire_moyennes.annee.Content;
+            moy_annee_classe_Emilien.Content = lire_moyennes.annee_classe.Content;
+            lire_moyennes.tri1.Content = "";
+            lire_moyennes.tri2.Content = "";
+            lire_moyennes.tri3.Content = "";
+            lire_moyennes.tri1_classe.Content = "";
+            lire_moyennes.tri2_classe.Content = "";
+            lire_moyennes.tri3_classe.Content = "";
             }
         }
     }
